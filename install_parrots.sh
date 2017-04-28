97332f3c7cb4f8481c4d77aba30e095fa6ac4abd
Kai Chen <chenkaidev@gmail.com>
2017-04-28 16:56:50 +0800

    modify parrots installation scripts

--- install_parrots.sh
+++ install_parrots.sh
@@ -8,10 +8,9 @@ cd $PARROTS_HOME
 if [ ! -d "$PARROTS_HOME/parrots.man" ]; then
     git clone git@github.com:ParrotsDL/parrots.man.git
     cd parrots.man
-    echo "export PARROTS_HOME=$PARROTS_HOME" >> $HOME/.profile
-    echo "export GTEST_ROOT=\$PARROTS_HOME/gtest" >> $HOME/.profile
-    echo "export PATH=\$PARROTS_HOME/parrots/tools:\$PATH" >> $HOME/.profile
-    echo "export LD_LIBRARY_PATH=\$PARROTS_HOME/parrots/build:\$PARROTS_HOME/PPL2/lib:\$LD_LIBRARY_PATH" >> $HOME/.profile
+    PARROTS_HOME=$PARROTS_HOME ./parrots-deps.sh
+    echo -e "\nexport PARROTS_HOME=$PARROTS_HOME" >> $HOME/.profile
+    echo "source \$PARROTS_HOME/parrots/scripts/env.sh" >> $HOME/.profile
 else
     cd parrots.man
     git pull origin master
@@ -19,7 +18,7 @@ fi
 
 pip install -r requirements.txt --user --upgrade
 bash parrots-deps.sh
-./paman update
+PARROTS_HOME=$PARROTS_HOME ./paman update
 PARROTS_HOME=$PARROTS_HOME GTEST_ROOT=$PARROTS_HOME/gtest ./paman build
 PARROTS_HOME=$PARROTS_HOME GTEST_ROOT=$PARROTS_HOME/gtest \
 LD_LIBRARY_PATH=$PARROTS_HOME/parrots/build:$PARROTS_HOME/PPL2/lib:$LD_LIBRARY_PATH ./paman test
